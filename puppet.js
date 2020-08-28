cd809dfc042d5e3114e4b3f4892be7741b1012e7
Nicolas Lattemann <56591727+nLatt@users.noreply.github.com>
2020-08-28 13:22:19 +0200

    can click on the "agree" button for cookies

--- puppet.js
+++ puppet.js
@@ -2,7 +2,23 @@ const puppeteer = require("puppeteer");
 
 (async () => {
 
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 200,
+  });
+
   const page = await browser.newPage();
 
+  await page.setViewport({
+    width: 640,
+    height: 480,
+    deviceScaleFactor: 1,
+  });
+
+  await page.goto("https://techcrunch.com/tag/series-a/");
+
+  page.click("[name='agree']");
+  page.waitFor(1500);
+  page.waitForSelector("a.post-block__title__link");
+  // page.click("a.post-block__title__link");
 })();
