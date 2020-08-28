b6dc92f18d60d8633000dbd8d9b2f2f7be614c49
Nicolas Lattemann <56591727+nLatt@users.noreply.github.com>
2020-08-28 13:53:38 +0200

    correctly navigates to the article and creates a pdf file

--- puppet.js
+++ puppet.js
@@ -2,9 +2,11 @@ const puppeteer = require("puppeteer");
 
 (async () => {
 
+  const website = "https://techcrunch.com/tag/series-a/"
+
   const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 200,
+    slowMo: 100,
   });
 
   const page = await browser.newPage();
@@ -15,10 +17,16 @@ const puppeteer = require("puppeteer");
     deviceScaleFactor: 1,
   });
 
-  await page.goto("https://techcrunch.com/tag/series-a/");
+  await page.goto(website);
 
   page.click("[name='agree']");
+
+  await page.waitForNavigation({ waitUntil: 'networkidle2' });
   page.waitFor(1500);
   page.waitForSelector("a.post-block__title__link");
+  // article_url = await page.$eval("a.post-block__title__link", elm => elm.getAttribute("href"));
+  article_url = await page.evaluate("document.querySelector('a.post-block__title__link').getAttribute('href')");
+  await page.goto("https://techcrunch.com" + article_url);
+  await page.pdf({path: "tech_articles.pdf", format: "A4"});
   // page.click("a.post-block__title__link");
 })();
