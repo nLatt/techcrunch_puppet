b54a6c7326dbc0ce8828aa5aca59126af1cc36d5
Nicolas Lattemann <56591727+nLatt@users.noreply.github.com>
2020-08-29 15:58:00 +0200

    script is correctly extracting the author's name and date of the article's creation

--- puppet.js
+++ puppet.js
@@ -23,15 +23,15 @@ const error = chalk.bold.red;
   await page.goto(website);
   page.click("[name='agree']");
   await page.waitForNavigation({ waitUntil: 'networkidle2' });
-  await page.once('load', () => console.log('✅ Main website is loaded'));
+  page.once('load', () => console.log('✅ Main website is loaded'));
   const article_url = await page.evaluate("document.querySelector('a.post-block__title__link').getAttribute('href')");
 
   await page.goto("https://techcrunch.com" + article_url);
-  await page.once('load', () => console.log('✅ First article is loaded'));
 
-  const article_text = await page.evaluate("document.querySelector('div.article-content').innerText");
+  const article_text = await page.evaluate("document.querySelector('div.article__byline').innerText");
   console.log(article_text)
 
+  await page.pdf({path: "generated_files\\tech_articles.pdf", format: "A4"});
   await browser.close();
   console.log(error("Browser Closed"));
 })();
